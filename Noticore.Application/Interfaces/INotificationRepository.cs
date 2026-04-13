706e526e362f1409161602b2a9a857a2952648d2
JC <jcdotnet@hotmail.com>
2026-04-13 11:18:00 +0200

    feat: implement notification repository

--- Noticore.Application/Interfaces/INotificationRepository.cs
+++ Noticore.Application/Interfaces/INotificationRepository.cs
@@ -8,5 +8,8 @@ namespace Noticore.Application.Interfaces
         Task<IEnumerable<Notification>> GetAllAsync();
         Task AddAsync(Notification notification);
         Task UpdateAsync(Notification notification);
+
+        // Note: Delete operation is intentionally omitted to maintain notification history and audit trail. 
+        // If removal is required, consider implementing a Soft Delete (IsDeleted) approach.
     }
 }
