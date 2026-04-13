548ae66dd3c7093b46c467c59b907ae8cdc327d2
JC <jcdotnet@hotmail.com>
2026-04-13 10:44:41 +0200

    feat: add notification repository interface to application layer

---
+++ Noticore.Application/Interfaces/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Noticore.Domain.Entities;
+
+namespace Noticore.Application.Interfaces
+{
+    public interface INotificationRepository
+    {
+        Task<Notification?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Notification>> GetAllAsync();
+        Task AddAsync(Notification notification);
+        Task UpdateAsync(Notification notification);
+    }
+}
