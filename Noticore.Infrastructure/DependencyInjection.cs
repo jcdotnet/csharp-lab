f6599345f9532fb8bdb678d4ec2f81f9c01c508e
JC <jcdotnet@hotmail.com>
2026-04-13 11:24:45 +0200

    feat: add infrastructure dependency injection extension

---
+++ Noticore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Noticore.Application.Interfaces;
+using Noticore.Infrastructure.Persistence;
+using Noticore.Infrastructure.Repositories;
+
+namespace Noticore.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            // Database Configuration
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            // Repositories
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+
+            return services;
+        }
+    }
+}
