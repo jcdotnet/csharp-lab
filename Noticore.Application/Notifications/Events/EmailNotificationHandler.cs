fd6ae36c1c5e7b68a160fa9f22bbb63e2242df48
JC <jcdotnet@hotmail.com>
2026-04-13 17:26:23 +0200

    fix: update notification status after successful delivery

--- Noticore.Application/Notifications/Events/EmailNotificationHandler.cs
+++ Noticore.Application/Notifications/Events/EmailNotificationHandler.cs
@@ -1,19 +1,34 @@
 ﻿using MediatR;
 using Noticore.Application.Interfaces;
+using Noticore.Domain.Enums;
 
 namespace Noticore.Application.Notifications.Events
 {
-    public class EmailNotificationHandler(IEmailService emailService) : INotificationHandler<NotificationCreatedEvent>
+    public class EmailNotificationHandler(
+        IEmailService emailService,
+        INotificationRepository repository
+      ) : INotificationHandler<NotificationCreatedEvent>
     {
         public async Task Handle(NotificationCreatedEvent notification, CancellationToken cancellationToken)
         {
             // Logic to send email only if the type is Email
             if (notification.Notification.Type == Domain.Enums.NotificationType.Email)
             {
-                await emailService.SendEmailAsync(
+                try
+                {
+                    await emailService.SendEmailAsync(
                     notification.Notification.Recipient,
                     notification.Notification.Title,
                     notification.Notification.Message);
+
+                    notification.Notification.Status = NotificationStatus.Sent;
+                }
+                catch (Exception)
+                {
+                    notification.Notification.Status = NotificationStatus.Failed;
+                }
+
+                await repository.UpdateAsync(notification.Notification);
             }
         }
     }
